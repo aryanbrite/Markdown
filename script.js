deb1a8fff173a912c58169075f83b10d55edb2e2
Aryan <aryanbrite@gmail.com>
2026-06-17 14:27:47 +0530

    added markdown praser

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 console.log("hellow");
 let editor = document.getElementById("editor");
 let prev = document.getElementById("prev")
-editor.addEventListener("input",function(){prev.innerText=editor.value});
+editor.addEventListener("input",function(){prev.innerHTML=marked.parse(editor.value)});
